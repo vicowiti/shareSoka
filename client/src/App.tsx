8567e63e6d64b8d3125d09d2136fbd4b3cae4e97
Victor Owiti <viowiti12@gmail.com>
2022-12-09 14:52:02 +0300

    Added the select room form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
+import SelectRoomPage from "./components/SelectRoomPage";
+import { io, Socket } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "./types/socketClientTypes";
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "http://localhost:4000"
+);
 
 function App() {
-  return <div>Hello</div>;
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  function joinRoom() {
+    if (username && room) {
+      socket.emit("join_room", { username, room });
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+        <Route
+          path="/select"
+          element={
+            <SelectRoomPage
+              setUsername={setUsername}
+              username={username}
+              room={room}
+              setRoom={setRoom}
+              socket={socket}
+            />
+          }
+        />
+        {/* <Route path="/" element={} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/components/SelectRoomPage.tsx
@@ -0,0 +1,52 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { Socket } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../types/socketClientTypes";
+
+interface SelectRoomPageProps {
+  username: string;
+  room: string;
+  socket: Socket<ServerToClientEvents, ClientToServerEvents>;
+  setRoom: Dispatch<SetStateAction<string>>;
+  setUsername: Dispatch<SetStateAction<string>>;
+}
+
+const SelectRoomPage = ({
+  username,
+  socket,
+  room,
+  setRoom,
+  setUsername,
+}: SelectRoomPageProps) => {
+  return (
+    <div>
+      <h1>SHARESOKA</h1>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <select onChange={(e) => setRoom(e.target.value)}>
+            <option>--Select Room--</option>
+            <option value="countries">Countries</option>
+            <option value="pl">Premier League</option>
+            <option value="liga">La Liga</option>
+            <option value="others">Others</option>
+
+            <option value="pl">Premier League</option>
+          </select>
+        </div>
+
+        <button type="submit">Join Room</button>
+      </form>
+    </div>
+  );
+};
+
+export default SelectRoomPage;
